3ce5bbcf61f2dbd4b6b27337a866785888589d70
t_md_tahir_21 <tahirbca@gmail.com>
2024-02-11 21:18:17 +0530

    New Feature Added | Typing Effect in Home Page

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ function updateTitle() {
 
 setInterval(updateTitle, 1000); // Adjust the interval as needed
 
+
+
+    const professions = ["Frontend Developer", "Graphics Designer", "Wordpress Developer"];
+    const professionElement = document.getElementById('profession');
+    let currentIndex = 0;
+    let currentText = '';
+    let letterIndex = 0;
+    let isDeleting = false;
+
+    function typeWriterEffect() {
+        const profession = professions[currentIndex];
+        if (!isDeleting) {
+            currentText = profession.substring(0, letterIndex + 1);
+            professionElement.textContent = currentText;
+            letterIndex++;
+            if (currentText === profession) {
+                isDeleting = true;
+                setTimeout(typeWriterEffect, 1000); // Pause at the end of typing
+            } else {
+                setTimeout(typeWriterEffect, 100); // Typing speed
+            }
+        } else {
+            currentText = profession.substring(0, letterIndex - 1);
+            professionElement.textContent = currentText;
+            letterIndex--;
+            if (currentText === '') {
+                isDeleting = false;
+                currentIndex = (currentIndex + 1) % professions.length;
+            }
+            setTimeout(typeWriterEffect, 50); // Deleting speed
+        }
+    }
+
+    typeWriterEffect();
+
